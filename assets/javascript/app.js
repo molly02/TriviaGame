64ca460b1c6c40c4e816e320dd34fb3f1e62c800
mp02 <molly_patterson@yahoo.com>
2019-07-12 16:33:32 -0500

    added bootstrap and html from tutoring session

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
 
-
+// timer counting down
+// form for questions, form javascript here
+// submit button
+// next page, results
+    // correct, incorrect, unanswered
+    
 
 
 
