3d8834a19229e2695d2a146f460312fe1e8a54a7
mp02 <molly_patterson@yahoo.com>
2019-07-16 19:10:42 -0500

    added html and initial variables

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,31 @@
 $(document).ready(function() {
 
 // timer counting down
+    // set timeout
+    // go to results page when timer goes out
+function timer () {
+
+
+var userChoice = "";
+var correct = 0;
+var incorrect = 0;
+var unanswer = 0;
+var timer = 30;
+
+}
 // form for questions, form javascript here
+    // onclick for buttons
+    // grab values of answers
+    // push values of answers into a single array
+    // containing 1, 2, or 3
+function counter (arr){
+// 
+
+}
 // submit button
 // next page, results
     // correct, incorrect, unanswered
-    
+
 
 
 
